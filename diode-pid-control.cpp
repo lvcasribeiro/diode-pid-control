4f23001a74a651ad68155a6a94ac9b29e0b051d5
Lucas Ribeiro <lucas.and.rib@gmail.com>
2024-01-03 09:54:13 -0300

    Adds handling for serial monitor readings.

--- diode-pid-control.cpp
+++ diode-pid-control.cpp
@@ -2,6 +2,9 @@
 const int ldr_pin = 15;
 const int transistor_pin = 5;
 
+// Global variables:
+int setpoint = 0;
+
 // Setup function:
 void setup() {
     pinMode(ldr_pin, INPUT);
@@ -10,4 +13,25 @@ void setup() {
     analogWrite(transistor_pin, 0);
 
     Serial.begin(115200);
+}
+
+// Loop function:
+void loop() {
+    if (Serial.available() > 0) {
+        readSerialMonitor();
+    }
+}
+
+// Serial monitor reading function:
+void readSerialMonitor() {
+    String user_input = Serial.readStringUntil('\n');
+    int new_setpoint = user_input.toInt();
+    
+    if (new_setpoint != 0) {
+        setpoint = new_setpoint;
+        Serial.print("- Setpoint changed to: ");
+        Serial.println(setpoint);
+    } else {
+        Serial.println("- Invalid input. Please enter a valid integer.");
+    }
 }
